9896cbb85242c8ed0bb576a354da4c9b733def0e
Steven Perfors <stevecanbuild@gmail.com>
2020-01-27 03:05:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(num)
+  num.sort do |a, b|
+    a <=> b 
+  end
+end
 
+def sort_array_desc(num)
+  num.sort do |a, b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(num)
+  num.sort{|x, y| x.length <=> y.length}
+  end
+end
+
+def swap_elements(num)
+  num[1], num[2] = num[2], num[1]
+end
+
+def reverse_array(num)
+  num.reverse 
+end
